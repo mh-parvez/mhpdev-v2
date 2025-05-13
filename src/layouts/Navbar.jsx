2ac317ff8b96648e71834c7e5738d5b01ae8e2c0
mh-parvez <mh.parvez.dev1@gmail.com>
2025-05-13 23:16:27 +0600

    new reusme added

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -3,69 +3,65 @@ import { navlinks } from "./../assets/data";
 import { useState } from "react";
 
 const Navbar = () => {
-	const [hamburger, setHamburger] = useState(false);
+  const [hamburger, setHamburger] = useState(false);
 
-	const hamburgerMenu = () => {
-		setHamburger(!hamburger);
-	};
+  const hamburgerMenu = () => {
+    setHamburger(!hamburger);
+  };
 
-	const pageUp = () => {
-		window.scrollTo({ top: (0, 0), behavior: "smooth" });
-	};
+  const pageUp = () => {
+    window.scrollTo({ top: (0, 0), behavior: "smooth" });
+  };
 
-	return (
-		<div>
-			<nav>
-				<h3 className='logo' onClick={pageUp}>
-					Parvez
-				</h3>
-				<ul>
-					{navlinks.map((item) => (
-						<li key={item.name}>
-							<a href={item.link}>{item.name}</a>
-						</li>
-					))}
-					<li>
-						<a
-							href='https://drive.google.com/file/d/17uttGl_osX8ecqVcV6GLLue99OxhqL1m/view'
-							target='_blank'>
-							Resume
-						</a>
-					</li>
-					<li>
-						<HiMenu
-							className='mobile-menu'
-							onClick={() => hamburgerMenu()}
-						/>
-					</li>
-				</ul>
-			</nav>
-			{/* mobile menu */}
-			<div
-				className={`mobile-nav ${
-					hamburger ? "open-menu" : "closed-menu"
-				}`}>
-				<span onClick={() => hamburgerMenu()}>
-					<HiX className='close-button' />
-				</span>
+  return (
+    <div>
+      <nav>
+        <h3 className="logo" onClick={pageUp}>
+          Parvez
+        </h3>
+        <ul>
+          {navlinks.map((item) => (
+            <li key={item.name}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+          <li>
+            <a
+              href="https://drive.google.com/file/d/1MrPVk_FJnpOz8hDE6-oNJNy5m8c-mtCo/view"
+              target="_blank"
+            >
+              Resume
+            </a>
+          </li>
+          <li>
+            <HiMenu className="mobile-menu" onClick={() => hamburgerMenu()} />
+          </li>
+        </ul>
+      </nav>
+      {/* mobile menu */}
+      <div className={`mobile-nav ${hamburger ? "open-menu" : "closed-menu"}`}>
+        <span onClick={() => hamburgerMenu()}>
+          <HiX className="close-button" />
+        </span>
 
-				<ul>
-					{navlinks.map((item) => (
-						<li key={item.name} onClick={() => hamburgerMenu()}>
-							<a href={item.link}>{item.name}</a>
-						</li>
-					))}
-					<li>
-						<a
-							href='https://drive.google.com/file/d/17uttGl_osX8ecqVcV6GLLue99OxhqL1m/view'
-							target='_blank'>
-							Resume
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+        <ul>
+          {navlinks.map((item) => (
+            <li key={item.name} onClick={() => hamburgerMenu()}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+          <li>
+            <a
+              href="https://drive.google.com/file/d/1MrPVk_FJnpOz8hDE6-oNJNy5m8c-mtCo/view"
+              target="_blank"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
